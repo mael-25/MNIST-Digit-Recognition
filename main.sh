f609c54aa9d2a49fd046225a7138b6fbc2b610c0
Mael <livet.yang@gmail.com>
2022-01-30 10:25:48 +0800

    Modif

--- main.sh
+++ main.sh
@@ -1,1 +1,6 @@
-p main.py --epochs=100 --batch-size=16 --learning-rate-type="automatic" --weight-decay=0.001
+python3.9 main.py \
+    --epochs=50 \
+    --batch-size=32 \
+    --learning-rate-type="automatic" \
+    --weight-decay=0.005 \
+    --dropout=0.005
